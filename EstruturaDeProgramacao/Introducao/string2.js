4e09012cbfb7cd813cef8aa4e0137c8aa6ac9579
joaovitor <joaovitorresende.jr@gmail.com>
2024-09-07 12:56:19 -0300

    adicao de introducao

---
+++ EstruturaDeProgramacao/Introducao/string2.js
@@ -0,0 +1,5 @@
+console.log("primeira linha \n segunda linha")
+console.log("o meu nome é 'joao'")
+console.log('o meu nome é "joao"')
+console.log(`uma multiplicacao ${5*3}`)
+console.log("O"+" meu "+" nome "+" é "+" joao")
